fc9557d2495bb063d35865fa580a8152d6c361e5
Admin <ngoctin2412test@gmail.com>
2022-12-10 14:04:29 +0700

    a6

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path, include
 
 from welcome.views import index, health
 
