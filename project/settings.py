8e6632661879b9b84c18277fde6e6c834bc5c357
Admin <ngoctin2412test@gmail.com>
2022-12-10 16:24:05 +0700

    a

--- project/settings.py
+++ project/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+ALLOWED_HOSTS = ["https://ve-xe-re-git-1-ngoctin2412test-dev.apps.sandbox.x8i5.p1.openshiftapps.com","localhost:8000"]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
