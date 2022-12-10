7f3e4773f611232b8496a3375eb0663c0f6b4198
Admin <ngoctin2412test@gmail.com>
2022-12-10 14:15:00 +0700

    a6

--- customers/urls.py
+++ customers/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from . import views
+from django.conf.urls import include, url
 
 app_name = 'customers'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('check/', views.checkCustomer, name='check'),
-    path('forgotPassword', views.forgotPassword, name='forgotPassword'),
-    path('userInfo', views.userInfo, name='userInfo'),
-    path('listCoach', views.listCoach, name='listCoach'),
-    path('datve', views.datve, name='datve'),
-    path('_login', views.loginView, name='login'),
-     path('quanlyve', views.quanlyveView, name='login'),
+    url(r'^$', views.index, name='index'),
+    # path('check/', views.checkCustomer, name='check'),
+    # path('forgotPassword', views.forgotPassword, name='forgotPassword'),
+    # path('userInfo', views.userInfo, name='userInfo'),
+    # path('listCoach', views.listCoach, name='listCoach'),
+    # path('datve', views.datve, name='datve'),
+    # path('_login', views.loginView, name='login'),
+    #  path('quanlyve', views.quanlyveView, name='login'),
 
 ]
