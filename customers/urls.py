94368893e966432f3175dd1f2b3186ac5334ceb6
Admin <ngoctin2412test@gmail.com>
2022-12-10 16:38:27 +0700

    a

--- customers/urls.py
+++ customers/urls.py
@@ -5,12 +5,12 @@ app_name = 'customers'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('check/', views.checkCustomer, name='check'),
-    # path('forgotPassword', views.forgotPassword, name='forgotPassword'),
-    # path('userInfo', views.userInfo, name='userInfo'),
-    # path('listCoach', views.listCoach, name='listCoach'),
-    # path('datve', views.datve, name='datve'),
-    # path('_login', views.loginView, name='login'),
-    # path('quanlyve', views.quanlyveView, name='login'),
+    path('check/', views.checkCustomer, name='check'),
+    path('forgotPassword', views.forgotPassword, name='forgotPassword'),
+    path('userInfo', views.userInfo, name='userInfo'),
+    path('listCoach', views.listCoach, name='listCoach'),
+    path('datve', views.datve, name='datve'),
+    path('_login', views.loginView, name='login'),
+    path('quanlyve', views.quanlyveView, name='login'),
 
 ]
